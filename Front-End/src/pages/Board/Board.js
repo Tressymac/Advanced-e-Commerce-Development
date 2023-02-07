3d80f242e5c64921b23d4bc08dd327ccdb525cb7
Jessica <jessicamac48@yahoo.com>
2023-02-06 23:07:08 -0500

    stuff

--- Front-End/src/pages/Board/Board.js
+++ Front-End/src/pages/Board/Board.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import useTaskFetcher from '../../hook/useTaskFetcher.js';
+import axios from 'axios';
 
 import Lane from '../../components/Lane/Lane.js';
 import '../Board/Board.css';
@@ -14,27 +15,56 @@ const lanes = [
 function Board( {apiURL} ) {
     const [tasks, setTasks] = useState([]);
     const [loading, error, taskdata] = useTaskFetcher(apiURL+ `/tasks`);
+    const [modifiedTask, setModifiedTask] = useState(null);
+
+    useEffect(() => {
+        console.log("I'm in the useEffect hook!");
+        console.log(modifiedTask);
+
+        // Define a function that calls the API with the updated task
+        const updateTask = async (task) => {
+            try {
+                const apiResponse = await axios.patch(apiURL + '/tasks/' + task.id, task)
+                await console.log(apiResponse.data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        if (modifiedTask != null) {
+            // This is where I want to write the code to change the task via the API
+            updateTask(modifiedTask);
+        } else {
+            console.log('Modified task is null');
+        }
+
+        return () => {
+            setModifiedTask(null);
+        }
+
+    }, [apiURL, modifiedTask]);
 
     //Event Handlers for dragging 
-    function onDragStart(event, taskId){
-        console.log('ID of task being dragged: ' + taskId)
-        event.dataTransfer.setData('taskid', taskId)
+    function onDragStart(event, taskId) {
+        console.log('ID of task being dragged: ' + taskId);
+        event.dataTransfer.setData('taskId', taskId);
     }
 
-    function onDragOver(event){
+    function onDragOver(event) {
         event.preventDefault();
     }
 
-    function onDrop(event, laneId){
-        //Retrive the id of the task being dropped
+    function onDrop(event, laneId) {
+        // Retrieve the id of the task being dropped
         const taskId = event.dataTransfer.getData('taskId');
-        console.log('Task ' + taskId + ' goes to lane ' + laneId);
+        console.log('Task ' + taskId + ' goes to Lane ' + laneId);
 
-        //uodate the list of task in state with the updated version of the dropped task
+        // Update the list of tasks in state with the updated version of the dropped task
         const updatedTasks = tasks.filter((task) => {
-            //If the current task is the task that was dropped, then update its lane ID
-            if(task.id.toString() === taskId){
-                task.lane = laneId
+            // IF the current task is the task that was dropped, update its lane id
+            if (task.id.toString() === taskId) {
+                task.lane = laneId;
+                setModifiedTask(task);
             }
             return task;
         });
