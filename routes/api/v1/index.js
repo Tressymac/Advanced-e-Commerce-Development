90cfa5df36686184bc692350f2000d39bf9d5bdb
Jessica <jessicamac48@yahoo.com>
2023-02-06 23:06:47 -0500

    stuff

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -5,29 +5,33 @@ const mongoose = require('mongoose');
 require('models/Tasks');
 const Task = mongoose.model('tasks');
 
-router.get('/tasks', async (req, res) => {
-    const filter = {};
-    const tasks = await Task.find(filter);
-    res.json(tasks);
+// Root route
+router.get('/', (req, res) => {
+    res.send('Root API route');
 });
 
-router.get('/', (req, res) => {
-    res.send('RootAPI route')
-    alert("hello")
+// Getting all tasks in database
+router.get('/tasks', async (req, res) => {
+    setTimeout(async function() {
+        const filter = {};
+        const tasks = await Task.find(filter);
+        console.log(tasks);
+        res.json(tasks);
+    }, 3000);
 });
 
 //Update a task in the databade
 router.patch('/tasks/:id', async (req, res) => {
     console.log(req.params.id);
 
-    //use mongoose schema for task; "findOneAndUpdate"
-    const filter = { id: parseInt(req.params.id) };
-    const updateDate = req.body;
-    const updatedDoc = await Task.findOneAndUpdate(filter, updateDate, {new: true});
+    // Use Mongoose schema for Task; "findOneAndUpdate"
+    const filter = {id: parseInt(req.params.id)};
+    const updateData = req.body;
+    const updatedDoc = await Task.findOneAndUpdate(filter, updateData, { new: true });
     console.log(updatedDoc);
 
     res.status(200);
-    res.json({updated_task: parseInt(req.params.id)});
+    res.json({ updated_task: parseInt(req.params.id)});
 });
 
 module.exports = router;
